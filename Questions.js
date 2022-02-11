20e1f3d274feba09270f1b7be70a35979c668baf
Sean <seanthewonderful@gmail.com>
2022-02-11 15:36:08 -0700

    initcommit

---
+++ Questions.js
@@ -0,0 +1,16 @@
+let list = ['a','b','c','d']
+list.pop()
+console.log(list)
+/*Is there any way to have a line of code recall or go back to
+a previous code line that executed before a permanent command did?
+In other words as above shown, if you .pop an array without assiging
+a variable to store the .pop, you lose the item that was popped.
+Can you at some future point tell the code to go back to a line of
+code before the .pop execution and basically "reset" the list? As 
+of now, it seems easier to just make it into a variable, but I'm
+thinking of a long code where the .pop is random and maybe in the
+future the user or coder wants to recall what the list looked like
+in the past. I am also thinking that if you contain all code within
+a () after the original array then you could maybe resolve it that
+way but idk how.
+*/
