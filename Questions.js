89e8856384ebc7ac97b5022fbc2052283222c16f
Sean <seanthewonderful@gmail.com>
2022-02-11 16:20:38 -0700

    commit

--- Questions.js
+++ Questions.js
@@ -1,6 +1,6 @@
-let list = ['a','b','c','d']
-list.pop()
-console.log(list)
+// let list = ['a','b','c','d']
+// list.pop()
+// console.log(list)
 /*Is there any way to have a line of code recall or go back to
 a previous code line that executed before a permanent command did?
 In other words as above shown, if you .pop an array without assiging
@@ -14,3 +14,45 @@ in the past. I am also thinking that if you contain all code within
 a () after the original array then you could maybe resolve it that
 way but idk how.
 */
+
+//print out every item in your list
+
+// let list = ['1','0','3','9','7','2']
+//iterate through each item in your list
+//for(let i=0; i<list.length; i++){
+  //print each item as I go
+  //stop when there are no more elements
+  //console.log(list[i])
+//}
+//given an array of numbers, print the
+//largest and smallest numbers in array:
+//Thinking process: Need to keep track of smallest and largest numbers. 
+//let smallest = list[0]
+//let largest = list[0]
+//Need to compare all numbers to all other numbers. Need to update smallest and largest as I go.
+// for(let i=0;i<list.length; i++){
+//   if(list[i]>largest){
+//     largest=list[i]
+//   }
+//   if(list[i]<smallest){
+//     smallest=list[i]
+//   }
+// }
+// console.log(largest)
+// console.log(smallest)
+//This doesnt print the list in order, it just prints the largest and smallest numbers in the list
+
+
+//Take password input
+const readline = require("readline")
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+reader.question("Do you like cats? y/n", function(answer){
+    if(answer=="y"){
+        console.log("Correct")
+    }else{
+        console.log("Incorrect")
+    }
+})
